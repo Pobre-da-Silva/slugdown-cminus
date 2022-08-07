560957797e2d5f0092b4aeb2a62b3d81641a1d25
Comunismno <109741514+Comunismno@users.noreply.github.com>
2022-08-07 16:30:00 +0100

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,11 @@
+#include <Windows.h>
+#include "Cabeçalho1.h"
+#include <iostream>
+
+int main() {
+	std::cout << "algo";
+	setSlugdownTime(2000);
+	getSlugdownTime();
+	slugdown();
+	std::cout << "algo";
+}
